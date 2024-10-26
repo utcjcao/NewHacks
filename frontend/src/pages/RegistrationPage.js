dcabdd8f27157d28091bb4801c3329e4fcd5bec9
RoyceLuo1216 <86991110+RoyceLuo1216@users.noreply.github.com>
2024-10-26 17:07:56 -0400

    Changed Frontend Layout

--- frontend/src/pages/RegistrationPage.js
+++ frontend/src/pages/RegistrationPage.js
@@ -5,15 +5,7 @@ import * as options from "../components/options"; // Make sure options.js has th
 const RegistrationPage = () => {
   // Define initial values for the form
   const [formValues, setFormValues] = useState({
-    county: "",
-    familySize: "",
-    houseType: "",
-    travelMeans: "",
-    youngInfants: "",
-    childCount: "",
-    pets: "",
-    petCount: "",
-    medicalInfo: ""
+
   });
 
   // Handler to update form values on input change
@@ -35,7 +27,7 @@ const RegistrationPage = () => {
   return (
     <div className="page-container">
       <div className="form-container">
-        <h1>Registration Form</h1>
+        <h1>Information</h1>
         <form onSubmit={handleSubmit}>
           <RegistrationFields values={formValues} onChange={handleChange} />
 
@@ -46,15 +38,6 @@ const RegistrationPage = () => {
         </form>
       </div>
 
-      <div className="sidebar">
-        <h2>Additional Features</h2>
-        <ul>
-          <li>Feature 1</li>
-          <li>Feature 2</li>
-          <li>Feature 3</li>
-          <li>Feature 4</li>
-        </ul>
-      </div>
     </div>
   );
 };
