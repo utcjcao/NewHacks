5e6b43e6b0f5d6e3cc3132e6ce5de56bac7ceadd
RoyceLuo1216 <86991110+RoyceLuo1216@users.noreply.github.com>
2024-10-27 10:31:27 -0400

    Storm Track for Milton

---
+++ frontend/src/pages/StormTrack.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StormTrack from "../components/Storm";
+
+const RoutePage = () => {
+  return (
+    <div className="page-container">
+      <h1 className="header">Route Info</h1>
+      <StormTrack />
+    </div>
+  );
+};
+
+export default StormTrackPage;
